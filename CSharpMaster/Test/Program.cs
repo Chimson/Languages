b9ade145ffb0f70e640ac33461cf7f8953fe6de4
Chimson <bharki@depaul.edu>
2023-06-29 22:01:47 -0500

    C# test

--- CSharpMaster/Test/Program.cs
+++ CSharpMaster/Test/Program.cs
@@ -1,7 +1,21 @@
-﻿class Program {
+﻿using System.Diagnostics;
+class Program {
   static void Main(string[] args) { 
-    String msg = "Hi";
-    int x = 10;
-    Console.WriteLine(msg + $"{x}"); 
+    try {
+      using (Process myProcess = new Process()) {
+        myProcess.StartInfo.UseShellExecute = false;
+        // You can start any process, HelloWorld is a do-nothing example.
+        myProcess.StartInfo.FileName = "/home/bharki/Repos/Languages/CSharpMaster/Test/trythis.py";
+        myProcess.StartInfo.CreateNoWindow = true;
+        myProcess.Start();
+        // This code assumes the process you are starting will terminate itself.
+        // Given that it is started without a window so you cannot terminate it
+        // on the desktop, it must terminate itself or you can do it programmatically
+        // from this application using the Kill method.
+      }
+    }
+    catch (Exception e) {
+      Console.WriteLine(e.Message);
+    }
   } 
 }
