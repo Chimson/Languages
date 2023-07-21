2ea2db45fbfab38e6a0151104402a9321d778d95
Chimson <bensonharkison10@hotmail.com>
2023-07-20 19:35:23 -0500

    CSharp SOLID examples

--- CSharpMaster/DesignPatterns.cs
+++ CSharpMaster/DesignPatterns.cs
@@ -1,16 +1,43 @@
 // Inspired by reading Design Patterns .NET 6 3rd Edition
-
 /* 
-SOLID Design Principles
-  SRP: Single Responsibility
-    one responsibility, therefore only one reason to change
   OCP: Open-Closed
     open for extension but close for modification
     extendable without modifying and recompiling
     interface/abstract everything
   LSP: Liskov Substitution
+    something that takes a Child type or Parent type, should take both without error 
   ISP: Interface Segregation
-  DIP: Dependence Inversion
+    for example: an IPrinter interface that contains, print(), fax(), scan()
+    really should split the methods into their own interfaces
+    then you can have a class that picks which interfaces it wants
 */
 
-// STOPPED AT PAGE 13 LISKOV
+SOLID DESIGN PRINCIPLES
+// SRP: Single Responsibility
+// class has one responsibility, therefore only one reason to change
+// can change the format style, without changing the Student class
+class Student {
+  public string? Name {init; get;}
+  public int ID {init; get;}
+}
+class IDNameFormat {
+  public static string FString(Student student) {
+    return $"#{student.ID}: {student.Name}";
+  }
+} 
+class NameIDFormat {
+  public static string FString(Student student) {
+    return $"{student.Name} - {student.ID}";
+  }
+} 
+
+class Program {
+  static void Main(string[] args) { 
+    Student ben = new Student {Name = "Ben Harki", ID = 1};
+    Console.WriteLine(IDNameFormat.FString(ben));
+    Console.WriteLine(NameIDFormat.FString(ben));
+  }
+}
+
+// TODO: FINISH EXAMPLES FOR SOLID PRINCIPLES
+// STOPPED AT PAGE 20 PARAMETER OBJECT
