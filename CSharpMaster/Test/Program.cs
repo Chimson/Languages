284a66fdd7dc385ea6887ac227b346be5ca7105e
Chimson <bensonharkison10@hotmail.com>
2023-08-04 22:02:54 -0500

    C# studd

--- CSharpMaster/Test/Program.cs
+++ CSharpMaster/Test/Program.cs
@@ -1,6 +1,18 @@
-﻿// TODO: implement an interface and use a where clause for a Type Parameter
+// asynchronous programming is like the "making breakfst analogy"
+// you would start cooking the bacon, while its cooking work on something else ...
+// different that parallel programing in that, you have multiple "cooks" or "threads" that are blocked to their task
+class MySleep {
+  public int Time {init; get;}
+  public static async int StartSleep(int i) {
+    await Thread.Sleep(i);   // in System.Threading
+    return i;
+  }
+}
 class Program {
   static void Main(string[] args) {   
+    Console.WriteLine("Hi");
+    MySleep one = new MySleep {Time = 20}; 
+    
   }
 }
 
